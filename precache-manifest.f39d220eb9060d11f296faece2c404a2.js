06ef2ca9aab2c2b588ffa2c13caa88c171d62e7f
Julien <julescript@outlook.com>
2020-05-17 22:24:42 +0300

    Updates

---
+++ precache-manifest.f39d220eb9060d11f296faece2c404a2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a2008499c48256f988df157e9090d1",
+    "url": "/spotifyndr/index.html"
+  },
+  {
+    "revision": "389cbb6d18a65237b523",
+    "url": "/spotifyndr/static/css/main.592080a7.chunk.css"
+  },
+  {
+    "revision": "44a02d6ddf72ff5c6fbe",
+    "url": "/spotifyndr/static/js/2.b10ba747.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spotifyndr/static/js/2.b10ba747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389cbb6d18a65237b523",
+    "url": "/spotifyndr/static/js/main.67c857ac.chunk.js"
+  },
+  {
+    "revision": "a0d949c4e7e4a3aa2bb6",
+    "url": "/spotifyndr/static/js/runtime-main.3db4c61e.js"
+  },
+  {
+    "revision": "7a172fc916ea57923b677f1fde0c359d",
+    "url": "/spotifyndr/static/media/back_icon.7a172fc9.svg"
+  },
+  {
+    "revision": "c8ccbc7b1c8087e7fc7f68f4d3787c21",
+    "url": "/spotifyndr/static/media/lineto-circular-black.c8ccbc7b.ttf"
+  },
+  {
+    "revision": "ea04057fd27e203b57359bfed0258b4f",
+    "url": "/spotifyndr/static/media/lineto-circular-bold.ea04057f.ttf"
+  },
+  {
+    "revision": "f25f54b0e3425c7ac5282f0a5890271f",
+    "url": "/spotifyndr/static/media/lineto-circular-book.f25f54b0.ttf"
+  },
+  {
+    "revision": "d71f06b9dc0c7402069697dcabbcb85b",
+    "url": "/spotifyndr/static/media/lineto-circular-medium.d71f06b9.ttf"
+  },
+  {
+    "revision": "da3ea075caad9df9c65bf3c64fc9c270",
+    "url": "/spotifyndr/static/media/logo_icon.da3ea075.svg"
+  },
+  {
+    "revision": "ae73e08efde79fe114005cc06a66fd4a",
+    "url": "/spotifyndr/static/media/logo_spotify.ae73e08e.svg"
+  },
+  {
+    "revision": "21cb713c4b5061c9de20bb5afaeec4f5",
+    "url": "/spotifyndr/static/media/star_empty.21cb713c.svg"
+  },
+  {
+    "revision": "765ace4eccc975b8b6798ef1859a9f1f",
+    "url": "/spotifyndr/static/media/star_filled.765ace4e.svg"
+  }
+]);
